6e571fb7f43fdb7fc0c83a83cc6a87bd0df5728d
harsh147780 <harshchaudhary1477@gmail.com>
2025-11-18 11:12:25 +0530

    Keep only jsClassAssignment in repo

--- jsOperatorAssignment/Q7.js
+++
@@ -1,6 +0,0 @@
-// Q7: Customer Feedback Processor
-let feedback = "Great product! Fast delivery and amazing sound quality!";
-let wordCount = feedback.split(" ").length;
-if (feedback.toLowerCase().includes("bad") || feedback.toLowerCase().includes("poor")) console.log("Needs Improvement");
-else console.log("Positive Feedback");
-console.log(`Word Count: ${wordCount}`);
